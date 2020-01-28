34633feaa37fc47f9c597b04e57b4f3ed1e6f0d2
ghpark <ghpark@dtnsm.com>
2020-01-28 18:09:09 +0900

    이메일 메일 발송 서식 적용 Self 상시 교육 기간 연장(1회만 가능하게) 추가

--- src/main/java/com/dtnsm/lms/controller/MyPageCvJdController.java
+++ src/main/java/com/dtnsm/lms/controller/MyPageCvJdController.java
@@ -1,6 +1,5 @@
 package com.dtnsm.lms.controller;
 
-import com.dtnsm.common.entity.JobDescriptionVersionFile;
 import com.dtnsm.common.entity.QUserJobDescription;
 import com.dtnsm.common.entity.Signature;
 import com.dtnsm.common.entity.UserJobDescription;
@@ -656,6 +655,4 @@ public class MyPageCvJdController {
 
         return "redirect:/mypage/jd/approved";
     }
-
-
 }
--- src/main/java/com/dtnsm/lms/service/MailService.java
+++ src/main/java/com/dtnsm/lms/service/MailService.java
@@ -2,17 +2,13 @@ package com.dtnsm.lms.service;
 
 import com.dtnsm.lms.domain.constant.BinderAlarmType;
 import com.dtnsm.lms.domain.constant.LmsAlarmCourseType;
-import com.dtnsm.lms.domain.constant.MailSendType;
 import com.dtnsm.lms.domain.datasource.MessageSource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
